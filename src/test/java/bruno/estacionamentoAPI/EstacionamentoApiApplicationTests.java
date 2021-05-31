80c634117fb15418780f582eff0a88d195b99a40
Bruno Campos <brunocamposfow@gmail.com>
2021-05-31 09:23:53 -0300

    Arquivos Iniciais

---
+++ src/test/java/bruno/estacionamentoAPI/EstacionamentoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package bruno.estacionamentoAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstacionamentoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
