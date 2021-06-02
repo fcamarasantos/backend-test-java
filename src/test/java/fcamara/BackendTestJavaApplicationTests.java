512d4b2a0ee8ba2fb78a496e9aa4329edb88fd72
Guilherme Camargo Gonzalez <guilherme.gonzales@fcamara.com.br>
2021-06-02 11:03:58 -0300

    build: config inicial projeto, pom.xml e application.properties

---
+++ src/test/java/fcamara/BackendTestJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package fcamara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendTestJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
