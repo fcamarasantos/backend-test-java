9d88d70dd077025380d1974656db8a6780178442
Guilherme Camargo Gonzalez <guilherme.gonzales@fcamara.com.br>
2021-06-02 17:52:40 -0300

    feat: Criação do script de insert sql e o repository

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO veiculo(marca, modelo, cor, placa, tipo) VALUES('VOLKSWAGEN','GOLF GTI','PRETO','ABC1D231','CARRO');
+INSERT INTO veiculo(marca, modelo, cor, placa, tipo) VALUES('VOLKSWAGEN','JETTA GLI','BRANCO','FGH5J231','CARRO');
+INSERT INTO veiculo(marca, modelo, cor, placa, tipo) VALUES('VOLKSWAGEN','FUSCA','VERMELHO','TYU1D456','CARRO');
+INSERT INTO veiculo(marca, modelo, cor, placa, tipo) VALUES('VOLKSWAGEN','TIGUAN','BRANCO','KIL1D331','CARRO');
