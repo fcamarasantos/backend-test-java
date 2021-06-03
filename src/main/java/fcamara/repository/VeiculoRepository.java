fd80f708e18b8a7dd39a8b2573ab78c23d41724b
Guilherme Camargo Gonzalez <guilherme.gonzales@fcamara.com.br>
2021-06-03 11:02:17 -0300

    refactor: correção de identação e tipo do Id

--- src/main/java/fcamara/repository/VeiculoRepository.java
+++ src/main/java/fcamara/repository/VeiculoRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import fcamara.model.Veiculo;
 
-public interface VeiculoRepository extends JpaRepository<Veiculo, Long>{
+public interface VeiculoRepository extends JpaRepository<Veiculo, String>{
 
 	Veiculo findByPlaca(String placa);
 
