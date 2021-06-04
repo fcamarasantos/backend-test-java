7ae42ae34c6b96c0e16c4d57c06b7cdb917643fe
BBrolesi <brunohsbrolesi@gmail.com>
2021-06-04 10:39:23 -0300

    chore: init spring boot project

---
+++ src/main/java/br/com/brunobrolesi/parking/ParkingApplication.java
@@ -0,0 +1,13 @@
+package br.com.brunobrolesi.parking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/brunobrolesi/parking/ParkingApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.brunobrolesi.parking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
