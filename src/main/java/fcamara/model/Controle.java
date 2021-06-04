b02db604d2e5344f045fe93d07f095d30d467c44
Guilherme Camargo Gonzalez <guilherme.gonzales@fcamara.com.br>
2021-06-04 16:32:36 -0300

    feat: Modelagem da entidade Controle, e ajuste no script data.sql

---
+++ src/main/java/fcamara/model/Controle.java
@@ -0,0 +1,87 @@
+package fcamara.model;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="controle")
+public class Controle {
+	
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@ManyToOne
+	private Veiculo veiculo;
+	
+	@ManyToOne
+	private Estacionamento estacionamento;
+	
+	private LocalDateTime datahora_entrada = LocalDateTime.now();
+	private LocalDateTime datahora_saida = null;
+	
+	public Controle() {
+	}
+	
+	public Controle(Controle controle) {
+		this.veiculo = controle.getVeiculo();
+		this.estacionamento = controle.getEstacionamento();
+		this.datahora_entrada = controle.getDatahora_entrada();
+		this.datahora_saida = controle.getDatahora_saida();
+	}
+
+	public Controle(Veiculo veiculo, Estacionamento estacionamento) {
+		this.veiculo = veiculo;
+		this.estacionamento = estacionamento;
+	}
+	
+	
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Veiculo getVeiculo() {
+		return veiculo;
+	}
+
+	public void setVeiculo(Veiculo veiculo) {
+		this.veiculo = veiculo;
+	}
+
+	public Estacionamento getEstacionamento() {
+		return estacionamento;
+	}
+
+	public void setEstacionamento(Estacionamento estacionamento) {
+		this.estacionamento = estacionamento;
+	}
+
+	public LocalDateTime getDatahora_entrada() {
+		return datahora_entrada;
+	}
+
+	public void setDatahora_entrada(LocalDateTime datahora_entrada) {
+		this.datahora_entrada = datahora_entrada;
+	}
+
+	public LocalDateTime getDatahora_saida() {
+		return datahora_saida;
+	}
+
+	public void setDatahora_saida(LocalDateTime datahora_saida) {
+		this.datahora_saida = datahora_saida;
+	}
+
+	
+}
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,3 +7,7 @@ INSERT INTO estacionamento(nome, cnpj, endereco, telefone, qtd_moto, qtd_carro) 
 INSERT INTO estacionamento(nome, cnpj, endereco, telefone, qtd_moto, qtd_carro) VALUES('Estacionamento do Leonardo', '45678912336985','Rua Washington Luiz, 456, SP', '11985624761',20,80);
 INSERT INTO estacionamento(nome, cnpj, endereco, telefone, qtd_moto, qtd_carro) VALUES('Estacionamento do Michelangelo', '14725836985214','Rua Marechal Kleber, 87, SP', '11982514376',50,15);
 INSERT INTO estacionamento(nome, cnpj, endereco, telefone, qtd_moto, qtd_carro) VALUES('Estacionamento do Donatelo', '75315985245625','Rua São Lucas Tadeu, 5, SP', '11988996475',100,70);
+INSERT INTO estacionamento(nome, cnpj, endereco, telefone, qtd_moto, qtd_carro) VALUES('Estacionamento do Splinter', '75123455245625','Rua Padre Afonso, 78, SP', '11988997410',0,0);
+
+INSERT INTO controle(datahora_entrada, datahora_saida, estacionamento_cnpj, veiculo_placa) VALUES(CURRENT_TIMESTAMP, null, '14253647586941', 'ABC1D231');
+INSERT INTO controle(datahora_entrada, datahora_saida, estacionamento_cnpj, veiculo_placa) VALUES(CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, '14253647586941', 'ABC1D231');
