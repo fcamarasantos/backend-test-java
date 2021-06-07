e7c3a79ba95cb4e67576723c9b1bcffd2c6bd271
Guilherme Camargo Gonzalez <guilherme.gonzales@fcamara.com.br>
2021-06-07 13:52:22 -0300

    feat: modelagem relatório veiculo e dto, e ajuste no script data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,6 +3,19 @@ INSERT INTO veiculo(marca, modelo, cor, placa, tipo) VALUES('VOLKSWAGEN','JETTA 
 INSERT INTO veiculo(marca, modelo, cor, placa, tipo) VALUES('VOLKSWAGEN','FUSCA','VERMELHO','TYU1D456','CARRO');
 INSERT INTO veiculo(marca, modelo, cor, placa, tipo) VALUES('VOLKSWAGEN','TIGUAN','BRANCO','KIL1D331','CARRO');
 
+INSERT INTO veiculo(marca, modelo, cor, placa, tipo) VALUES('HONDA','CIVIC SI','PRETO','ATE1Y234','CARRO');
+INSERT INTO veiculo(marca, modelo, cor, placa, tipo) VALUES('HONDA','FIT','BRANCO','JUK8L487','CARRO');
+INSERT INTO veiculo(marca, modelo, cor, placa, tipo) VALUES('HONDA','ACCORD','VERMELHO','RFV1V123','CARRO');
+INSERT INTO veiculo(marca, modelo, cor, placa, tipo) VALUES('HONDA','WRX','BRANCO','KIQ8H587','CARRO');
+
+INSERT INTO veiculo(marca, modelo, cor, placa, tipo) VALUES('TOYOTA','COROLLA','PRATA','POI7P897','CARRO');
+INSERT INTO veiculo(marca, modelo, cor, placa, tipo) VALUES('TOYOTA','ETIOS','BRANCO','USX8Z512','CARRO');
+
+INSERT INTO veiculo(marca, modelo, cor, placa, tipo) VALUES('KAWAZAKI','H2R','CARBONO','UJZ8S258','MOTO');
+INSERT INTO veiculo(marca, modelo, cor, placa, tipo) VALUES('YAMAHA','MT-09','PRETO','URV5U167','MOTO');
+INSERT INTO veiculo(marca, modelo, cor, placa, tipo) VALUES('YAMAHA','MT-07','AZUL','PLM9O514','MOTO');
+INSERT INTO veiculo(marca, modelo, cor, placa, tipo) VALUES('YAMAHA','MT-03','BRANCO','UHV7Y158','MOTO');
+
 INSERT INTO estacionamento(nome, cnpj, endereco, telefone, qtd_moto, qtd_carro) VALUES('Estacionamento do Rafael', '14253647586941','Rua Teodoro Sampaio, 12, SP', '11985634578',10,40);
 INSERT INTO estacionamento(nome, cnpj, endereco, telefone, qtd_moto, qtd_carro) VALUES('Estacionamento do Leonardo', '45678912336985','Rua Washington Luiz, 456, SP', '11985624761',20,80);
 INSERT INTO estacionamento(nome, cnpj, endereco, telefone, qtd_moto, qtd_carro) VALUES('Estacionamento do Michelangelo', '14725836985214','Rua Marechal Kleber, 87, SP', '11982514376',50,15);
