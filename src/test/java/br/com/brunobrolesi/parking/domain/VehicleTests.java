79c5856546c44718af5150b6dd22a9de734e4208
BBrolesi <brunohsbrolesi@gmail.com>
2021-06-08 09:06:40 -0300

    test: init vehicle test implementation

---
+++ src/test/java/br/com/brunobrolesi/parking/domain/VehicleTests.java
@@ -0,0 +1,52 @@
+package br.com.brunobrolesi.parking.domain;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class VehicleTests {
+
+    @Test
+    void manufacturerMustBeInLowerCase() {
+        Assertions.fail();
+    }
+
+    @Test
+    void modelMustBeInLowerCase() {
+        Assertions.fail();
+    }
+
+    @Test
+    void colorMustBeInLowerCase() {
+        Assertions.fail();
+    }
+
+    @Test
+    void vehicleTypeMustBeInLowerCase() {
+        Assertions.fail();
+    }
+
+    @Test
+    void vehicleTypeMustValid() {
+        Assertions.fail();
+    }
+
+    @Test
+    void licensePlateMustBeInUpperCase() {
+        Assertions.fail();
+    }
+
+    @Test
+    void licensePlateMustBeValid(){
+        Assertions.fail();
+    }
+
+    @Test
+    void yearMustHaveFourDigits() {
+        Assertions.fail();
+    }
+
+    @Test
+    void yearMustBeValid() {
+        Assertions.fail();
+    }
+}
