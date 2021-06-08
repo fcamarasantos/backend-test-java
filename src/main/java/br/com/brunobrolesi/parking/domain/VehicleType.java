394edc1d8e4c0801f6197c26960c96c5d382b3fb
BBrolesi <brunohsbrolesi@gmail.com>
2021-06-08 12:05:45 -0300

    feat: create vehicle type enum

---
+++ src/main/java/br/com/brunobrolesi/parking/domain/VehicleType.java
@@ -0,0 +1,36 @@
+package br.com.brunobrolesi.parking.domain;
+
+public enum VehicleType {
+    CARRO(1, "carro"),
+    MOTO(2, "moto");
+
+    private int id;
+    private String  description;
+
+    VehicleType(int id, String  description) {
+        this.id = id;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public static VehicleType toEnum(Integer id) {
+        if (id == null) {
+            return null;
+        }
+
+        for (VehicleType type: VehicleType.values()) {
+            if(id.equals(type.getId())) {
+                return type;
+            }
+        }
+
+        throw new IllegalArgumentException("Código de veículo invalido: " + id);
+    }
+}
