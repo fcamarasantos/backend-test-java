7a0d917d9677ca171fc815ac05fb979c7bf6200f
BBrolesi <brunohsbrolesi@gmail.com>
2021-06-09 09:10:37 -0300

    feat: list vehicles in /veiculos endpoint

--- src/main/java/br/com/brunobrolesi/parking/resources/VehiclesResource.java
+++ src/main/java/br/com/brunobrolesi/parking/resources/VehiclesResource.java
@@ -1,15 +1,39 @@
 package br.com.brunobrolesi.parking.resources;
 
+import br.com.brunobrolesi.parking.domain.Vehicle;
+import br.com.brunobrolesi.parking.domain.VehicleType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/veiculos")
 public class VehiclesResource {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String listar() {
-        return "Rest está funcionando";
+    public List<Vehicle> listVehicles() {
+        Vehicle vehicle1 = new Vehicle(
+                "FORD",
+                "Fusion",
+                "2018",
+                "Preto",
+                "ALX8182",
+                VehicleType.CARRO);
+
+        Vehicle vehicle2 = new Vehicle(
+                "Volkswagen",
+                "Gol",
+                "2018",
+                "Branco",
+                "ALB1234",
+                VehicleType.CARRO);
+
+        List<Vehicle> vehicles = new ArrayList<>();
+        vehicles.add(vehicle1);
+        vehicles.add(vehicle2);
+        return vehicles;
     }
 }
