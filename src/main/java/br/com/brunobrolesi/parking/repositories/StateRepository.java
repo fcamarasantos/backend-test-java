aa60a17077fd8923ac91b78ca5c40896429f47ff
BBrolesi <brunohsbrolesi@gmail.com>
2021-06-11 08:49:45 -0300

    feat: create state respository

---
+++ src/main/java/br/com/brunobrolesi/parking/repositories/StateRepository.java
@@ -0,0 +1,10 @@
+package br.com.brunobrolesi.parking.repositories;
+
+import br.com.brunobrolesi.parking.domain.State;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StateRepository extends JpaRepository <State, Integer> {
+
+}
