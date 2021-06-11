a9d378b56ae75a8d0b69de994635a9adb3124c14
BBrolesi <brunohsbrolesi@gmail.com>
2021-06-11 10:06:45 -0300

    feat: create parking entity

---
+++ src/main/java/br/com/brunobrolesi/parking/domain/Parking.java
@@ -0,0 +1,84 @@
+package br.com.brunobrolesi.parking.domain;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.*;
+
+@Entity
+public class Parking implements Serializable {
+    private static final long SerialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String cnpj;
+    private String name;
+
+    @OneToMany(mappedBy = "parking")
+    private List<Address> addresses = new ArrayList<>();
+
+    @ElementCollection
+    @CollectionTable(name = "phone")
+    private Set<String> phones = new HashSet<>();
+
+    public Parking() {}
+
+    public Parking(Integer id, String cnpj, String name) {
+        this.id = id;
+        this.cnpj = cnpj;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Address> getAddresses() {
+        return addresses;
+    }
+
+    public void setAddresses(List<Address> addresses) {
+        this.addresses = addresses;
+    }
+
+    public Set<String> getPhones() {
+        return phones;
+    }
+
+    public void setPhones(Set<String> phones) {
+        this.phones = phones;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Parking parking = (Parking) o;
+        return Objects.equals(id, parking.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
