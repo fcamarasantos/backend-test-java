4e50075f509b248b98ff5d19c317a1ec9421a0fc
BBrolesi <brunohsbrolesi@gmail.com>
2021-07-13 12:02:37 -0300

    feat: create tickets forms

---
+++ src/main/java/br/com/brunobrolesi/parking/controller/form/EntryTicketForm.java
@@ -0,0 +1,13 @@
+package br.com.brunobrolesi.parking.controller.form;
+
+public class EntryTicketForm {
+    private String vehicleLicensePlate;
+
+    public String getVehicleLicensePlate() {
+        return vehicleLicensePlate;
+    }
+
+    public void setVehicleLicensePlate(String vehicleLicensePlate) {
+        this.vehicleLicensePlate = vehicleLicensePlate;
+    }
+}
---
+++ src/main/java/br/com/brunobrolesi/parking/controller/form/ExitTicketForm.java
@@ -0,0 +1,13 @@
+package br.com.brunobrolesi.parking.controller.form;
+
+public class ExitTicketForm {
+    private Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
