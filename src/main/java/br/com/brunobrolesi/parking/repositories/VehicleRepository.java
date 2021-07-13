4ad4a773f260b7d1c0aea5170aa128c48548fc86
BBrolesi <brunohsbrolesi@gmail.com>
2021-07-13 12:04:22 -0300

    feat: find by license plate

--- src/main/java/br/com/brunobrolesi/parking/repositories/VehicleRepository.java
+++ src/main/java/br/com/brunobrolesi/parking/repositories/VehicleRepository.java
@@ -2,9 +2,14 @@ package br.com.brunobrolesi.parking.repositories;
 
 import br.com.brunobrolesi.parking.model.Vehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface VehicleRepository extends JpaRepository <Vehicle, Integer> {
 
+    @Query("select v from Vehicle v where license_plate = ?1")
+    Optional<Vehicle> findByLicensePlate(String vehiclePlate);
 }
