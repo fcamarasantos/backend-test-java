0ed5427cb7e71906369cff165c5193697d48f613
William Jonathan <willyeager01@gmail.com>
2021-08-30 15:27:46 -0300

    start project in spring starter

---
+++ src/main/java/br/com/williamjonathan/parking/ParkingApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.williamjonathan.parking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/williamjonathan/parking/ParkingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.williamjonathan.parking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
