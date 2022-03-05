df6a4e2a9f7d16fd85fc0629cd3fbe28fa7dae31
GustavoJPereira <gustavojosepereira2021@gmail.com>
2022-03-05 15:21:01 -0300

    build: inicia o marven e instala as dependências do spring

---
+++ src/main/java/br/com/backendtestjava/backendtestjava/BackendTestJavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.backendtestjava.backendtestjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendTestJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendTestJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/backendtestjava/backendtestjava/BackendTestJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.backendtestjava.backendtestjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendTestJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
