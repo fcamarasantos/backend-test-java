1eac7625685338b4ae3fff129a2aeb276f5369e6
Carlos Gizbert <carlos.gizbert@fcamara.com.br>
2022-05-02 20:52:43 -0300

    :sparkles: feat: Modulos Estabelecimentos e Veiculos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EstabelecimentosModule } from './estabelecimentos/estabelecimentos.module';
+import { VeiculosModule } from './veiculos/veiculos.module';
 
 @Module({
-  imports: [],
+  imports: [EstabelecimentosModule, VeiculosModule],
   controllers: [AppController],
   providers: [AppService],
 })
