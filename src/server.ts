12b98d8f04c4542707947c4b7bcef48bb8f465a4
Aline Avelino <aline.savelino@gmail.com>
2022-07-08 19:37:50 -0300

    ♻ refactor: rename and move dbConnection

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { routes } from "./routes";
-import { runDb } from "./services/connection";
+import { runDb } from "./config/dbConnection";
 
 const app = express();
 const port = 8080;
