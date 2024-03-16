3f5a0b8ba7d05551d2ae364e3a0a021a2bb0a9e2
iajor <rjai.neto@gmail.com>
2024-03-15 21:41:36 -0300

    feat: repository updated

--- restApiParkingLot/src/main/java/org/restapiparkinglot/restapiparkinglot/repository/VehicleRepository.java
+++ restApiParkingLot/src/main/java/org/restapiparkinglot/restapiparkinglot/repository/VehicleRepository.java
@@ -4,6 +4,10 @@ import org.restapiparkinglot.restapiparkinglot.model.Vehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface VehicleRepository extends JpaRepository<Vehicle, Integer> {
+    Optional<Vehicle> findByLicensePlate(String licensePlate);
+
 }
