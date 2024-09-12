fcf8c2d9455f9b2a860169ad839ab2d1af3d2fd1
pedro.dantas <pedro.dantas@sap.ce.gov.br>
2024-09-12 18:43:55 -0300

    spring security config

---
+++ src/main/java/com/fcamara/parking/api/config/security/SecurityConfig.java
@@ -0,0 +1,52 @@
+package com.fcamara.parking.api.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .authorizeHttpRequests(registry -> {
+                    registry.requestMatchers("/home").permitAll();
+                    registry.requestMatchers("/parking/**").hasRole("ADMIN");
+                    registry.requestMatchers("/vehicles/**", "/establishment/**").hasRole("USER");
+                    registry.anyRequest().authenticated();
+                })
+                .formLogin(AbstractAuthenticationFilterConfigurer::permitAll)
+                .build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails normalUser = User.builder()
+                .username("pedro.lucas")
+                .password("$2a$12$pLlEDlW7J3LJMLMl0Uv8Xu.NO1TYDvrMmIpoDhpHZ3So65XlsR.Vy")
+                .roles("USER")
+                .build();
+        UserDetails adminUser = User.builder()
+                .username("admin")
+                .password("$2a$12$4MVGfzHJ2C370at3MTGHdeX6z/kon2X5KbVWZTGfqjWBhj.KnQBuC")
+                .roles("ADMIN", "USER")
+                .build();
+        return new InMemoryUserDetailsManager(normalUser, adminUser);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
