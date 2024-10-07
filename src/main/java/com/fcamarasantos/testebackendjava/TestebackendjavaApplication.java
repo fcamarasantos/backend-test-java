0e5563952358ae21f36be2a0d7cf581358582813
Lino Mota <linomota0@gmail.com>
2024-10-07 16:38:39 -0400

    chore: criando projeto, adicionando dependencias e configurando conexao ao banco de dados

---
+++ src/main/java/com/fcamarasantos/testebackendjava/TestebackendjavaApplication.java
@@ -0,0 +1,13 @@
+package com.fcamarasantos.testebackendjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestebackendjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestebackendjavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fcamarasantos/testebackendjava/TestebackendjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fcamarasantos.testebackendjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestebackendjavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
